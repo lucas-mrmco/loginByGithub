fcac237d664a232d62785826de9eaf2d733de4f7
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2022-09-19 16:43:25 +0200

    add reset and log out

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,8 @@ import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/Supaba
 	    <input type="password" required v-model="passwd" ><br>
       <button v-on:click="register()">Sign Up</button>
       <button v-on:click="login()">Sign In</button>
+      <button v-on:click="reset()">Reset</button>
+      <button v-on:click="logout()">Sign out</button>
       <p>
       <label id="status"> You are not yet connected </label><br>  
       </p>
@@ -38,34 +40,58 @@ export default {
   methods: {  
     //this method allows a new user to sign up the system. Once done, the user receives an email
     //asking for account validation. Once the validation made the user is added to the system
-    async register(){ 
-      try { 
-        const { user, session, error } = await supabase.auth.signUp({ 
-          email: this.email, 
-          password: this.passwd, 
-        }); 
-        if (error) throw error; 
-        document.getElementById('status').innerHTML='Please validate the received email !' 
-      } catch (error) { 
-        alert(error.error_description || error.message); 
-      }  
+    async register(){
+      try {
+        const { user, session, error } = await supabase.auth.signUp({
+          email: this.email,
+          password: this.passwd,
+        });
+        if (error) throw error;
+        document.getElementById('status').innerHTML='Please validate the received email !'
+      } catch (error) {
+        alert(error.error_description || error.message);
+      } 
     },
     //this method allows the already registred user to log in the system.
-
     async login(){
-      try { 
-        const { user, session, error } = await supabase.auth.signIn({ 
-          email: this.email, 
-          password: this.passwd, 
-        });if (error) throw error; 
-        document.getElementById('status').innerHTML='You are now logged !' 
-      } catch (error) { 
-        alert(error.error_description || error.message); 
-      }  
-    }
-  }}
-  
+      try {
+        const { user, session, error } = await supabase.auth.signIn({
+          email: this.email,
+          password: this.passwd,
+        });
+        if (error) throw error;
+        document.getElementById('status').innerHTML='You are now logged !'
+      } catch (error) {
+        alert(error.error_description || error.message);
+      } 
+    },
+    async reset() {
+      const { data, error } = await supabase.auth.api.resetPasswordForEmail(
+        this.email
+      )
+    },
+    async logout() {
+      const { error } = await supabase.auth.signOut();
+      if (error) throw error;
+      document.getElementById('status').innerHTML = 'You are now logged out !'
+    },
+  },
+  mounted() {
+    supabase.auth.onAuthStateChange(async (event, session) => {
+      if (event == 'PASSWORD_RECOVERY') {
+        const newPassword = prompt('What would you like your new password to be?')
+        const { data, error } = await supabase.auth.update({
+          password: newPassword,
+        })
+        if (data) alert('Password updated successfully!')
+        if (error) alert('There was an error updating your password.')
+      }
+    })
+  }
+}  
 </script>
+
+  
 
 <!-- <style>
 @import './assets/base.css';
