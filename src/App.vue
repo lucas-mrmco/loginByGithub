700fb141fe9b5137259a34c5d0d54a6b06a3326e
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2022-09-26 16:20:28 +0200

    code importé pour auth google

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,13 @@ import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/Supaba
 </script>
 
 <template>    
-  <header>
-    <router-link to="/">Go to Home</router-link>
-    <img alt="Logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <div class="wrapper" id="signOut">
-      <div><SignIn msg="User, please sign in !" /></div>
-      <label>email: </label><br>
-	    <input type="email" required v-model="email" placeholder="username@domain.tld"><br>
-	    <label>password: </label><br>
-	    <input type="password" required v-model="passwd" ><br>
-      <button v-on:click="register()">Sign Up</button>
-      <button v-on:click="login()">Sign In</button>
-      <button v-on:click="reset()">Reset</button>
-      <button v-on:click="logout()">Sign out</button>
-      <p>
-      <label id="status"> You are not yet connected </label><br>  
-      </p>
-    </div>
-  </header>
-  
-  <main>
-    
-  </main>
+  <h1>{{ msg }}</h1> 
+  <p> 
+    Please login if you have an account or register : 
+  </p> 
+  <button @click="login()">Sign In</button><br> 
+  <button @click="logout()">Sign Out</button><br> 
+  <label id="status">You are not yet logged !  </label> 
 </template>
 
 <script>
@@ -35,60 +20,42 @@ const SUPABASE_URL = 'https://ufexjukbdzuneolctojt.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmZXhqdWtiZHp1bmVvbGN0b2p0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM1OTU4NjMsImV4cCI6MTk3OTE3MTg2M30.iow6grnMT1H2Uui1rhO7uN05r3B2kau3zaP2Q5ahpOM'
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
+supabase.auth.onAuthStateChange((event, session) => { 
+  if(session==null){ 
+    document.getElementById('status').innerHTML='You are not logged !!!'; 
+  } else{ 
+    //alert('session value: ' + JSON.stringify(session)) 
+    document.getElementById('status').innerHTML='You are logged with the email: ' + session.user.email; 
+  } 
+})
 
 export default {
   methods: {  
-    //this method allows a new user to sign up the system. Once done, the user receives an email
-    //asking for account validation. Once the validation made the user is added to the system
-    async register(){
-      try {
-        const { user, session, error } = await supabase.auth.signUp({
-          email: this.email,
-          password: this.passwd,
-        });
-        if (error) throw error;
-        document.getElementById('status').innerHTML='Please validate the received email !'
-      } catch (error) {
-        alert(error.error_description || error.message);
-      } 
-    },
-    //this method allows the already registred user to log in the system.
-    async login(){
-      try {
-        const { user, session, error } = await supabase.auth.signIn({
-          email: this.email,
-          password: this.passwd,
-        });
-        if (error) throw error;
-        document.getElementById('status').innerHTML='You are now logged !'
-      } catch (error) {
-        alert(error.error_description || error.message);
-      } 
-    },
-    async reset() {
-      const { data, error } = await supabase.auth.api.resetPasswordForEmail(
-        this.email
-      )
-    },
-    async logout() {
-      const { error } = await supabase.auth.signOut();
-      if (error) throw error;
-      document.getElementById('status').innerHTML = 'You are now logged out !'
-    },
-  },
-  mounted() {
-    supabase.auth.onAuthStateChange(async (event, session) => {
-      if (event == 'PASSWORD_RECOVERY') {
-        const newPassword = prompt('What would you like your new password to be?')
-        const { data, error } = await supabase.auth.update({
-          password: newPassword,
-        })
-        if (data) alert('Password updated successfully!')
-        if (error) alert('There was an error updating your password.')
-      }
-    })
+  //this method allows to release the connexion with the Google account 
+async logout(){ 
+      try { 
+        const { user, session, error } = await supabase.auth.signOut(); 
+        if (error) throw error; 
+        document.getElementById('status').innerHTML='You are disconnected !' 
+      } catch (error) { 
+        alert(error.error_description || error.message); 
+      }  
+    }, 
+    //this method allows to log in the system using Google provider 
+     
+async login(){ 
+      try { 
+        const { user, session, error } = await supabase.auth.signIn({ 
+          provider: 'google', 
+        }); 
+        if (error) throw error; 
+      } catch (error) { 
+        alert(error.error_description || error.message); 
+      }  
+    }
   }
-}  
+}
+ 
 </script>
 
   
